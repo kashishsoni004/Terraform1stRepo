d1e290b8c9bca1b3dbc342661e7e19bb37842bba
kashish <kashish003474@gmail.com>
2025-12-30 18:48:33 +0530

    day-1

---
+++ Day-1/variables.tf
@@ -0,0 +1,12 @@
+variable "ami_id" {
+    description = "The AMI ID for the EC2 instance"
+    type        = string
+    default     = ""
+  
+}
+variable "type" {
+    description = "The type of instance to use"
+    type        = string
+    default     = ""
+  
+}
