d6c5cdf86ffee33ed943c60cc9c676e4f1f15e05
kashish <kashish003474@gmail.com>
2026-01-04 10:20:26 +0530

    day-4

--- Day-4-remote-state/main.tf
+++ Day-4-remote-state/main.tf
@@ -1,9 +1,16 @@
 resource "aws_instance" "name" {
     ami           = "ami-068c0051b15cdb816"
     instance_type ="t2.micro"
-
+    
     tags = {
         Name = "Firstec2"
     }
     
+}
+resource "aws_instance" "test" {
+    ami           = "ami-068c0051b15cdb816"
+    instance_type ="t2.micro"
+    tags = {
+        Name = "Secondec2"
+}
 }
