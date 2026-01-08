fcb2d1d4c766ab0bf10d1d981de7baa0d38891b2
kashish <kashish003474@gmail.com>
2026-01-08 12:21:10 +0530

    day-4

---
+++ Day-4-remote-state-lock/main.tf
@@ -0,0 +1,37 @@
+resource "aws_instance" "name" {
+    ami           = "ami-068c0051b15cdb816"
+    instance_type ="t2.medium"
+    
+    tags = {
+        Name = "First2"
+    }
+    
+}
+resource "aws_instance" "test" {
+    ami           = "ami-068c0051b15cdb816"
+    instance_type ="t2.medium"
+    tags = {
+        Name = "second2"
+}
+}
+# resource "aws_s3_bucket" "name" {
+#     bucket = "kshghdbdhdhjchxgxxhcjjc"
+  
+# }
+# resource "aws_instance" "mahi" {
+#     ami           = "ami-068c0051b15cdb816"
+#     instance_type ="t2.large"
+#     tags = {
+#         Name = "thirdece2"
+#     } 
+# }
+# resource "aws_instance" "bhawna" {
+#     ami           = "ami-068c0051b15cdb816"
+#     instance_type ="t2.micro"
+    
+#     tags = {
+#         Name = "Fourthec2"
+#     }
+
+  
+# }
