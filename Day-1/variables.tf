74d2939cea7cb0a5a1cb1ad2516f81fd94d0da70
Kashish soni <159767573+kashishsoni004@users.noreply.github.com>
2026-02-01 19:02:41 +0530

    Set default values for ami_id and type variables

--- Day-1/variables.tf
+++ Day-1/variables.tf
@@ -1,12 +1,12 @@
 variable "ami_id" {
     description = "The AMI ID for the EC2 instance"
     type        = string
-    default     = ""
+    default     = "ami-0532be01f26a3de55"
   
 }
 variable "type" {
     description = "The type of instance to use"
     type        = string
-    default     = ""
+    default     = "t2.micro"
   
 }
